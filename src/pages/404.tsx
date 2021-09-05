d75892279bb21565a94b7f1022ac6a7d87e921f2
Avi <pireads@gmail.com>
2021-09-05 10:51:42 -0700

    added 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Link } from "gatsby";
+
+import Header from "../components/shared/Header";
 
 const NotFoundPage = () => {
   return (
     <div>
-      <Link to="/">Go home</Link>.
+      <Header />
+      <div className="container">
+        <h3>That page doesn't exist yet, but here's a haiku:</h3>
+        <p>Error: 404</p>
+        <p>Link not found, like the next line</p>
+        <p className="transparent">Or is it hiding?</p>
+      </div>
     </div>
   );
 };
